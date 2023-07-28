152c7db4848bccda238079d5980c38ba6486a352
James Cook <cookjws@gmail.com>
2023-07-28 19:32:43 +0100

    indentation of Memo test

--- test/integrals/Memoisation.jl
+++ test/integrals/Memoisation.jl
@@ -13,106 +13,106 @@ Random.seed!(0)
 
 @testset "Memoisation" begin
   fs = (identity, y->Dual(y, 1), y->Dual(y, -1))
-@testset "Testing perp memoisation logic" begin
-  @testset "Maxwellian" begin
-    kr, ki = rand(2)
-    vth = rand()
-    for n in -2:2
-      for f in fs
-        a = perpendicular(vth, f(kr + im*ki), abs(n))
-        for x in f.((kr+im*ki, kr-im*ki, -kr+im*ki, -kr-im*ki))
-          cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
-          b = perpendicular(vth, x, n)
-          if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-            @show n, sign(real(x)), sign(imag(x))
-            @show round.(b ./ cp(a))
+  @testset "Testing perp memoisation logic" begin
+    @testset "Maxwellian" begin
+      kr, ki = rand(2)
+      vth = rand()
+      for n in -2:2
+        for f in fs
+          a = perpendicular(vth, f(kr + im*ki), abs(n))
+          for x in f.((kr+im*ki, kr-im*ki, -kr+im*ki, -kr-im*ki))
+            cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
+            b = perpendicular(vth, x, n)
+            if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+              @show n, sign(real(x)), sign(imag(x))
+              @show round.(b ./ cp(a))
+            end
+            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+            @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
           end
-          @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-          @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
+          a = perpendicular(vth, f(kr), abs(n))
+          for x in f.((-kr, kr))
+            cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
+            b = perpendicular(vth, x, n)
+            if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+              @show n, sign(real(x)), sign(imag(x))
+              @show round.(b ./ cp(a))
+            end
+            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+            @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
+          end
         end
-        a = perpendicular(vth, f(kr), abs(n))
-        for x in f.((-kr, kr))
-          cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
-          b = perpendicular(vth, x, n)
-          if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-            @show n, sign(real(x)), sign(imag(x))
-            @show round.(b ./ cp(a))
+      end
+    end
+    @testset "Generic" begin
+      species = RingBeamSpecies(rand(6)...)
+      kr, ki = rand(2) # the positive parts
+      for n in -2:2
+        for f in fs
+          x = f(kr + im*ki)
+          config = Configuration(Wavenumber(kz=1.0, k⊥=x))
+          a = perpendicular(species, config, abs(n))
+          for x in f.((kr+im*ki, -kr+im*ki, kr-im*ki, -kr-im*ki)) # permutations
+            cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
+            config = Configuration(Wavenumber(kz=1.0, k⊥=x))
+            b = perpendicular(species, config, n)
+            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+            @test all(b .== cp(cp(b)))
           end
-          @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-          @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
+          config = Configuration(Wavenumber(kz=1.0, k⊥=f(kr))) # real positive
+          a = perpendicular(species, config, abs(n))
+          for x in f.((-kr, kr)) # real only permutations
+            config = Configuration(Wavenumber(kz=1.0, k⊥=x))
+            cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
+            b = perpendicular(species, config, n)
+            @test all(b .== cp(a))
+            @test all(b .== cp(cp(b)))
+          end
         end
       end
     end
   end
-  @testset "Generic" begin
-    species = RingBeamSpecies(rand(6)...)
-    kr, ki = rand(2)
-    for n in -2:2
+  @testset "Testing perp memoisation" begin
+    for species ∈ (MaxwellianSpecies(rand(5)...), RingBeamSpecies(rand(6)...))
       for f in fs
-        x = f(kr + im*ki)
-        config = Configuration(Wavenumber(kz=1.0, k⊥=x))
-        a = perpendicular(species, config, abs(n))
-        for x in f.((kr+im*ki, -kr+im*ki, kr-im*ki, -kr-im*ki))
-          cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
-          config = Configuration(Wavenumber(kz=1.0, k⊥=x))
-          b = perpendicular(species, config, n)
-          @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-          @test all(b .== cp(cp(b)))
-        end
-        config = Configuration(Wavenumber(kz=1.0, k⊥=f(kr)))
-        a = perpendicular(species, config, abs(n))
-        for x in f.((-kr, kr))
-          config = Configuration(Wavenumber(kz=1.0, k⊥=x))
-          cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
-          b = perpendicular(species, config, n)
-          @test all(b .== cp(a))
-          @test all(b .== cp(cp(b)))
-        end
-      end
-    end
-  end
-end
-@testset "Testing perp memoisation" begin
-  for species ∈ (MaxwellianSpecies(rand(5)...), RingBeamSpecies(rand(6)...))
-    for f in fs
-      for kr ∈ (rand(), -rand()), ki ∈ (rand(), -rand())
-        for B in (true, false)
-          for n ∈ -2:2
+        for kr ∈ (rand(), -rand()), ki ∈ (rand(), -rand())
+          for B in (true, false)
             K = Wavenumber(kz=1.0, k⊥=f((B ? kr+im*ki : kr)))
             config = Configuration(K)
-            a = perpendicular(species, config, n)
-            perpmem = LMV.constructperpendicular_memoised(species, config)
-            for _ ∈ 1:2
-              b = perpmem(species, config, n)
-              c = perpmem(species, config, n)
-              @test all(isapprox.(b, a, rtol=sqrt(eps())))
-              @test all(b .== c)
+            for n ∈ -2:2
+              a = perpendicular(species, config, n)
+              perpmem = LMV.constructperpendicular_memoised(species, config)
+              for _ ∈ 1:2
+                b = perpmem(species, config, n)
+                c = perpmem(species, config, n)
+                @test all(isapprox.(b, a, rtol=sqrt(eps())))
+                @test all(b .== c)
+              end
             end
           end
         end
       end
     end
   end
-end
-@testset "@inferred" begin
-  for species ∈ (MaxwellianSpecies(rand(5)...), RingBeamSpecies(rand(6)...))
-    n = 0
-    K = Wavenumber(kz=1.0, k⊥=rand())
-    config = Configuration(K)
-    perpmem = LMV.constructperpendicular_memoised(species, config)
-    try
-      @inferred perpendicular(species, config, n)
-      @test true
-    catch
-      @test false
-    end
-    try
-      @inferred perpmem(species, config, n)
-      @test true
-    catch
-      @test false
+  @testset "@inferred" begin
+    for species ∈ (MaxwellianSpecies(rand(5)...), RingBeamSpecies(rand(6)...))
+      n = 0
+      K = Wavenumber(kz=1.0, k⊥=rand())
+      config = Configuration(K)
+      perpmem = LMV.constructperpendicular_memoised(species, config)
+      try
+        @inferred perpendicular(species, config, n)
+        @test true
+      catch
+        @test false
+      end
+      try
+        @inferred perpmem(species, config, n)
+        @test true
+      catch
+        @test false
+      end
     end
   end
-end
 end
 
