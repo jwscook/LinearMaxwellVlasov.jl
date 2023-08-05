8cc302b243b1dc29d90df62fbe08e5dbe80e5ba5
James Cook <cookjws@gmail.com>
2023-08-05 21:34:51 +0100

    breaks at ratio of 16

--- src/distributionfunctions/FHyperGeomRings.jl
+++ src/distributionfunctions/FHyperGeomRings.jl
@@ -24,21 +24,19 @@ struct FHyperGeomRing{T<:Number, U<:Number, V<:Number
   α::T
   p::V
   vchar::V
-  invnormconst::V
+  lognormconst::V
   _uniqueid::UInt64
   function FHyperGeomRing(vth::T, vd::U) where {T<:Number, U<:Number}
     _uniqueid = hash((vth, vd), hash(:FHyperGeomRing))
     @assert vth > 0.0
     vchar = sqrt(vd^2 + vth^2)
-    @show t = sqrt(2) * vth # factor of √2 requied to make this similar the FRing
-    @show α = 1 / t^2
+    t = sqrt(2) * vth # factor of √2 requied to make this similar the FRing
+    α = 1 / t^2
     p = 2 * vd^2 / t^2
-    #invnormconst = 1 / exp(log(π * t^2) + p * log(t/vchar) + lgamma((p + 1)/2))
-    invnormconst = 1 / (π * t^2 * (t/vchar)^p * gamma((p + 2)/2))
-    @assert isfinite(invnormconst)
-    @assert invnormconst > 0
+    lognormconst = log(π * t^2) + p * log(t/vchar) + lgamma((p + 2)/2)
+    @assert isfinite(lognormconst)
     V = promote_type(T, U)
-    return new{T, U, V}(vth, vd, α, p, vchar, invnormconst, _uniqueid)
+    return new{T, U, V}(vth, vd, α, p, vchar, lognormconst, _uniqueid)
   end
 end
 
@@ -46,7 +44,7 @@ end
 #exp(log(a*v^p) + log(exp(y))) = exp(log(a*v^p) + y)) =
 #exp(log(a*v^p) + y)) = exp(log(a) + p * log(v) + y)) =
 function (f::FHyperGeomRing)(v::T, ∂F∂v::Bool=false) where {T<:Number}
-  output = exp(-f.α * v^2 + f.p * log(v / f.vchar) + log(f.invnormconst))
+  output = exp(-f.α * v^2 + f.p * log(v / f.vchar) - f.lognormconst)
   if ∂F∂v
     output *= (f.p / v - 2 * f.α * v)
   end
@@ -63,39 +61,41 @@ function AbramSteg66335(λ, μ::Unsigned, ν::Unsigned, α, β)
   @assert real(ν + λ + μ) > 0
   @assert real(α) > 0
   halfsum = (ν+λ+μ)/2
+#  @show -β^2/α, α^(-halfsum), β^(ν + μ)
   output = 1 / 2.0^(ν+μ+1) * α^(-halfsum) * β^(ν + μ)
   output *= exp(lgamma(halfsum) - lgamma(μ + 1) - lgamma(ν + 1))
   output *= HypergeometricFunctions.pFq(
     ((ν + μ + 1)/2, (ν + μ + 2)/2, halfsum),
     (μ + 1, ν + 1, μ + ν + 1), -β^2 / α)
+  @assert isfinite(output)
   return output
 end
 
 function integrate(f::FHyperGeomRing, kernel::T,
     ∂F∂v::Bool, tol::Tolerance=Tolerance()
     ) where {T<:Function}
-  β = kernel.k⊥_Ω # units of 1 / velocity, use a normalisation
+  β = kernel.k⊥_Ω
   μ, ν = kernel.μν
   sgnμ, μ = (μ < 0) ? (isodd(μ) ? -1 : 1, -μ) : (1, μ)
   sgnν, ν = (ν < 0) ? (isodd(ν) ? -1 : 1, -ν) : (1, ν)
   sgn = sgnμ * sgnν
   λ = kernel.power + f.p + 1
-  α = f.α
-  @show α, β, λ
-  #@show α = f.α / β^2 # noramlised α (α has units of 1/velocity^2)
-  #@show β /= β # noramlised β
+  normalisation = sqrt(f.α)
+  α = f.α / normalisation^2 # normalised α (α has units of 1/velocity^2)
+  β /= normalisation # normalise β
+  @assert isfinite(normalisation^λ)
   output = if ∂F∂v
     # f = (v/u)^p * exp(-v^2/t^2)
-    # df/dv = (p / v - 2 * v / t^2) * (v/u)^p * exp(-v^2/t^2)
-    i1 = AbramSteg66335(λ - 1, Unsigned(μ), Unsigned(ν), α, β) * f.p
-    i2 = AbramSteg66335(λ + 1, Unsigned(μ), Unsigned(ν), α, β) * 2f.α # α = 1 / t^2
+    # df/dv = (p / v - 2 * v / t^2) * (v/u)^p * exp(-v^2/t^2) # α = 1 / t^2
+    factor = 1 / normalisation^λ
+    f1 = factor * normalisation
+    f2 = factor / normalisation
+    i1 = AbramSteg66335(λ - 1, Unsigned(μ), Unsigned(ν), α, β) * f.p * f1
+    i2 = AbramSteg66335(λ + 1, Unsigned(μ), Unsigned(ν), α, β) * 2f.α * f2
     (i1 - i2)
   else
-    AbramSteg66335(λ, Unsigned(μ), Unsigned(ν), α, β)
+    AbramSteg66335(λ, Unsigned(μ), Unsigned(ν), α, β) / normalisation^λ
   end
-  @show output, f.invnormconst, 1 / f.vchar^f.p, sgn
-  @show output * f.invnormconst * 1 / f.vchar^f.p * sgn
-  return 2π * output * f.invnormconst / f.vchar^f.p * sgn
-  #return output * exp(log(f.invnormconst) - f.p * log(f.vchar)) * sgn
+  return output * exp(log(2π) - f.p * log(f.vchar) - f.lognormconst) * sgn
 end
 
--- test/distributionfunctions/FHyperGeomRings.jl
+++ test/distributionfunctions/FHyperGeomRings.jl
@@ -7,9 +7,12 @@ const LMV = LinearMaxwellVlasov
 
 
 @testset "FHyperGeomRings" begin
-  for _ in 1:10
-    vth = 1 + rand()
-    vd = vth / 10 #10 + 10 * rand()
+  for z in 0:2:26
+
+  @testset "Ratio $z" begin
+    for _ in 1:10
+    vth = rand()
+    vd = z * vth# + 2 * rand()
 
     normalring = LMV.FRing(vth, vd)
     ring = LMV.FHyperGeomRing(vth, vd)
@@ -17,15 +20,19 @@ const LMV = LinearMaxwellVlasov
     @test QuadGK.quadgk(v->2π*v*ring(v), 0, 12 * (vth + vd))[1] ≈ 1
     @test LMV.is_normalised(ring)
     deriv = rand(Bool)
+    n = rand(-10:10)
+    m = n + rand(-1:1)
     kernel = LMV.PerpendicularKernel(rand() * vth,
-      Pair(rand(-3:3), rand(-3:3)), rand(0:5))
+      Pair(n, m), rand(0:3))
     result = LMV.integrate(ring, kernel, deriv)
-    resultnormal = LMV.integrate(normalring, kernel, deriv)
-    expected = QuadGK.quadgk(v->kernel(v)*ring(v, deriv), 0, Inf, rtol=10eps())[1]
-    expectednormal = QuadGK.quadgk(v->kernel(v)*normalring(v, deriv), 0, Inf, rtol=10eps())[1]
+    expected = QuadGK.quadgk(v->kernel(v)*ring(v, deriv), 0, Inf, rtol=eps())[1]
     @test result ≈ expected
-    @show resultnormal, result
-    @show expectednormal, expected
+    #resultnormal = LMV.integrate(normalring, kernel, deriv)
+    #expectednormal = QuadGK.quadgk(v->kernel(v)*normalring(v, deriv), 0, Inf, rtol=10eps())[1]
+    #@show resultnormal, result
+    #@show expectednormal, expected
+  end
+  end
   end
  
 end
