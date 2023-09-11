8388936ee70d8a221857f4a20f3770655e39fb58
James Cook <cookjws@gmail.com>
2023-09-11 20:24:37 +0100

    works without needing residues

--- src/tensors/CoupledVelocity.jl
+++ src/tensors/CoupledVelocity.jl
@@ -132,46 +132,39 @@ function coupledvelocity(S::AbstractCoupledVelocitySpecies, C::Configuration)
   integrand(vz⊥) = integrand(vz⊥[1], vz⊥[2])
   function integrand(vz, v⊥)
     a = (ω - kz * vz) / Ω
-    b = k⊥ * v⊥ / Ω
-    c = kz * vz / ω
-    Ja = besselj(a, b)
-    J_a = besselj(-a, b)
-    Jad = (besselj(a - 1, b) - besselj(a + 1, b)) / 2
-    J_ad = (besselj(-a - 1, b) - besselj(-a + 1, b)) / 2
+    z = k⊥ * v⊥ / Ω
+    Ja = besselj(a, z)
+    J_a = besselj(-a, z)
+    Jad = (besselj(a - 1, z) - besselj(a + 1, z)) / 2
+    J_ad = (besselj(-a - 1, z) - besselj(-a + 1, z)) / 2
     π_sinπa = π / sinpi(a)
     dfdvz = DualNumbers.dualpart(S(Dual(vz, 1), v⊥))
     dfdv⊥ = DualNumbers.dualpart(S(vz, Dual(v⊥, 1)))
-    F⊥ = v⊥ * (dfdv⊥ * (1 - c) + c * dfdvz)
-    ω_Ω = ω / Ω
-    A0 = ω_Ω * π_sinπa * Jad * J_ad * F⊥
-    A1 = ω_Ω * π_sinπa / b^2 * Ja * J_a * F⊥
-    A2 = ω_Ω * π_sinπa * a / b^2 * Ja * J_ad * F⊥
-    A⊥ = ω_Ω * a / b^2 * F⊥
-    A3 = ω_Ω * π_sinπa * Ja * J_a
-    A4 = ω_Ω * π_sinπa * a * Ja * J_a * F⊥
-    A5 = ω_Ω * π_sinπa * Ja * J_ad * F⊥
-    A6 = A4
-    A7 = A5
-    sinψ, cosψ = sincos(ψ)
-    sin²ψ = sinψ^2
-    cos²ψ = 1 - sin²ψ
-    sin2ψ, cos2ψ = sincos(2ψ)
-    Kxx = A0 * sin²ψ + A1 * cos²ψ - A⊥ * cos2ψ
-    # Kxx = A1 + sin²ψ * A0
-    Kyy = A0 * cos²ψ + A1 * sin²ψ + A⊥ * cos2ψ
-    Kxy = im * A2 + (A1 - A0) * sin2ψ / 2 + A⊥ * (im - sin2ψ)
-    Kxz = cosψ * A4 + sinψ * A5
-    Kyz = -im * A2 + (A1 - A0) * sin2ψ / 2 - A⊥ * (im + sin2ψ)
-    Kzz = A3
-    return @MArray [Kxx Kxy Kxz; -Kxy Kyy Kyz; -Kxz -Kyz Kzz]
+    Q = a * π_sinπa * J_a * Ja # Eq 33
+    Qd = a * π_sinπa * (J_ad * Ja + J_a * Jad) # Eq 33
+    Xzz = 2π * Ω / ω * vz * (v⊥ * dfdvz - vz * dfdv⊥) # Eq 34, part
+    U = (kz / ω * v⊥ * dfdvz  + (1 - kz / ω * vz) * dfdv⊥) # Eq 4
+    T11 = a / z^2 * (Q - 1) * U
+    T12 = im / 2z * Qd * U
+    T13 = (Q - 1) / z * vz / v⊥ * U
+    T21 = -T12
+    T22 = (π_sinπa * J_ad * Jad + a / z^2) * U
+    T23 = im / 2 * (dfdvz / a + k⊥ / ω * (v⊥ * dfdvz - vz * dfdv⊥) / z) * Qd # Eq 28, 32, 9
+    T31 = T13
+    T32 = -T23 #im / 2a * Qd
+    T33 = Q / a * (vz / v⊥)^2 * U
+    T = @MArray [T11 T12 T13; T21 T22 T23; T31 T32 T33]
+    Xij = (2π * v⊥^2) .* T # Eq 34, part
+    Xij[3, 3] += Xzz
+    return ω / Ω * Xij # Eq 34
   end
 
-  pole = Pole(C.frequency, C.wavenumber, 0, S.Ω)
-  polefix = wavedirectionalityhandler(pole)
+  lower = max(S.F.lower, eps())
+
   function integral2D()
     ∫dvrdθ(vrθ) = vrθ[1] * integrand(parallelperpfrompolar(vrθ))
     return first(HCubature.hcubature(∫dvrdθ,
-      (S.F.lower, -π / 2), (S.F.upper, π / 2), initdiv=64,
+      (lower, -π / 2), (S.F.upper, π / 2), initdiv=64,
       rtol=C.options.quadrature_tol.rel, atol=C.options.quadrature_tol.abs))
   end
 
@@ -185,10 +178,13 @@ function coupledvelocity(S::AbstractCoupledVelocitySpecies, C::Configuration)
     return output
   end
 
+  pole = Pole(C.frequency, C.wavenumber, 0, S.Ω)
+  polefix = wavedirectionalityhandler(pole)
+
   function principal(v⊥)
     @assert !iszero(kz)
     ∫folded = foldnumeratoraboutpole(x->integrand((x, v⊥)), float(pole))
-    output = first(QuadGK.quadgk(∫dvz_kz_folded, S.F.lower, S.F.upper, order=32,
+    output = first(QuadGK.quadgk(∫folded, lower, S.F.upper, order=32,
         atol=C.options.quadrature_tol.abs,
         rtol=max(eps(), C.options.quadrature_tol.rel / 10)))
     @assert !any(isnan, output)# "v⊥ = $v⊥, output = $output"
@@ -198,33 +194,33 @@ function coupledvelocity(S::AbstractCoupledVelocitySpecies, C::Configuration)
   function coupledresidue(v⊥)
     # this started life in relativistic version - can it be simplified?
     ∫dvz(x) = integrand((x, v⊥))
-    ppradius = (iszero(imag(pole)) ? abs(pole) : abs(imag(pole))) * sqrt(eps())
-    pp = principalpartadaptive(∫dvz, pole, ppradius, 64,
-      C.options.summation_tol, Nmax=2048)
-    output = polefix.(residue(pp, polefix(pole)))
-    output = sign(real(kz)) .* real(output) .+ im .* imag(output)
-    @assert !any(isnan, output)# "v⊥ = $v⊥, pp = $pp, pole = $pole"
+      ppradius = (iszero(imag(pole)) ? abs(pole) : abs(imag(pole))) * sqrt(eps())
+      pp = principalpartadaptive(∫dvz, pole, ppradius, 64,
+        C.options.summation_tol, Nmax=2048)
+      output = polefix.(residue(pp, polefix(pole)))
+      output = sign(real(kz)) .* real(output) .+ im .* imag(output)
+      @assert !any(isnan, output)# "v⊥ = $v⊥, pp = $pp, pole = $pole"
     return output
   end
 
   function integralsnested1D(∫dv⊥::T, nrm=1) where T
-    return first(QuadGK.quadgk(∫dv⊥, S.F.lower, S.F.upper, order=32,
+    return first(QuadGK.quadgk(∫dv⊥, lower, S.F.upper, order=32,
       atol=max(C.options.quadrature_tol.abs,
                C.options.quadrature_tol.rel * nrm / 2),
       rtol=C.options.quadrature_tol.rel))
   end
 
-  @show isreal(pole), iszero(kz)
-  result = if isreal(pole) && iszero(kz)
+#  @show isreal(kz * ω), iszero(kz)
+  result = if isreal(kz * ω) && iszero(kz)
     integralsnested1D(principalzerokz)
-  elseif isreal(pole)# && !iszero(kz)
+  elseif isreal(kz * ω)# && !iszero(kz)
     pp = integralsnested1D(principal)
     pp .+ integralsnested1D(coupledresidue, norm(pp))
   elseif !iszero(kz) # && !isreal(pole)
     i2d = integral2D()
     i2d .+ integralsnested1D(coupledresidue, norm(i2d))
   else # iszero(kz) && !isreal(pole)
-    integral2D()
+    integral2D() # works
   end
   return result
 end
--- test/tensors/CoupledVsSeparable.jl
+++ test/tensors/CoupledVsSeparable.jl
@@ -43,50 +43,50 @@ Random.seed!(0)
         iszero(K) && continue
         config = Configuration(F, K)
         config.options = Options(quadrature_rtol=1.0e-9, summation_rtol=1e-8)
-        outputS = LMV.contribution(separable, config)
-        outputC = LMV.contribution(coupled, config)
+        #outputS = LMV.contribution(separable, config)
+        #outputC = LMV.contribution(coupled, config)
 
-        @test separable(0.0, 0.0) ≈ coupled(0.0, 0.0)
+        #@test separable(0.0, 0.0) ≈ coupled(0.0, 0.0)
 
-        @testset "real, $σ, $kz" begin
-          @test real(outputC[1,1])≈real(outputS[1,1]) rtol=rtol atol=atol
-          @test real(outputC[1,2])≈real(outputS[1,2]) rtol=rtol atol=atol
-          @test real(outputC[1,3])≈real(outputS[1,3]) rtol=rtol atol=atol
-          @test real(outputC[2,1])≈real(outputS[2,1]) rtol=rtol atol=atol
-          @test real(outputC[2,2])≈real(outputS[2,2]) rtol=rtol atol=atol
-          @test real(outputC[2,3])≈real(outputS[2,3]) rtol=rtol atol=atol
-          @test real(outputC[3,1])≈real(outputS[3,1]) rtol=rtol atol=atol
-          @test real(outputC[3,2])≈real(outputS[3,2]) rtol=rtol atol=atol
-          @test real(outputC[3,3])≈real(outputS[3,3]) rtol=rtol atol=atol
-        end
-        @testset "imag, $σ, $kz" begin
-          @test imag(outputC[1,1])≈imag(outputS[1,1]) rtol=rtol atol=atol
-          @test imag(outputC[1,2])≈imag(outputS[1,2]) rtol=rtol atol=atol
-          @test imag(outputC[1,3])≈imag(outputS[1,3]) rtol=rtol atol=atol
-          @test imag(outputC[2,1])≈imag(outputS[2,1]) rtol=rtol atol=atol
-          @test imag(outputC[2,2])≈imag(outputS[2,2]) rtol=rtol atol=atol
-          @test imag(outputC[2,3])≈imag(outputS[2,3]) rtol=rtol atol=atol
-          @test imag(outputC[3,1])≈imag(outputS[3,1]) rtol=rtol atol=atol
-          @test imag(outputC[3,2])≈imag(outputS[3,2]) rtol=rtol atol=atol
-          @test imag(outputC[3,3])≈imag(outputS[3,3]) rtol=rtol atol=atol
-        end
+        #@testset "real, $σ, $kz" begin
+        #  @test real(outputC[1,1])≈real(outputS[1,1]) rtol=rtol atol=atol
+        #  @test real(outputC[1,2])≈real(outputS[1,2]) rtol=rtol atol=atol
+        #  @test real(outputC[1,3])≈real(outputS[1,3]) rtol=rtol atol=atol
+        #  @test real(outputC[2,1])≈real(outputS[2,1]) rtol=rtol atol=atol
+        #  @test real(outputC[2,2])≈real(outputS[2,2]) rtol=rtol atol=atol
+        #  @test real(outputC[2,3])≈real(outputS[2,3]) rtol=rtol atol=atol
+        #  @test real(outputC[3,1])≈real(outputS[3,1]) rtol=rtol atol=atol
+        #  @test real(outputC[3,2])≈real(outputS[3,2]) rtol=rtol atol=atol
+        #  @test real(outputC[3,3])≈real(outputS[3,3]) rtol=rtol atol=atol
+        #end
+        #@testset "imag, $σ, $kz" begin
+        #  @test imag(outputC[1,1])≈imag(outputS[1,1]) rtol=rtol atol=atol
+        #  @test imag(outputC[1,2])≈imag(outputS[1,2]) rtol=rtol atol=atol
+        #  @test imag(outputC[1,3])≈imag(outputS[1,3]) rtol=rtol atol=atol
+        #  @test imag(outputC[2,1])≈imag(outputS[2,1]) rtol=rtol atol=atol
+        #  @test imag(outputC[2,2])≈imag(outputS[2,2]) rtol=rtol atol=atol
+        #  @test imag(outputC[2,3])≈imag(outputS[2,3]) rtol=rtol atol=atol
+        #  @test imag(outputC[3,1])≈imag(outputS[3,1]) rtol=rtol atol=atol
+        #  @test imag(outputC[3,2])≈imag(outputS[3,2]) rtol=rtol atol=atol
+        #  @test imag(outputC[3,3])≈imag(outputS[3,3]) rtol=rtol atol=atol
+        #end
 
         config.options = Options(quadrature_rtol=1.0e-4, summation_rtol=1e-8)
         t0 = @elapsed summed = LMV.contribution(separable, config)
-        try
-        t1 = @elapsed newberger = LMV.coupledvelocity(coupled, config)
-        ratio = newberger ./ summed
-        for i in eachindex(ratio)
-          @show ratio[i]
-        end
-        @show t1 / t0
-        @testset "newberger, $σ, $kz" begin
-          @test isapprox(summed, newberger)
-        end
-        #t0 = @elapsed LMV.converge(n->LMV.coupledvelocity(coupled, config, n))
-        #@show t1 / t0
-      catch
-      end
+        #try
+          t1 = @elapsed newberger = LMV.coupledvelocity(coupled, config)
+          ratio = newberger ./ summed
+          for i in eachindex(ratio)
+            @show ratio[i]
+          end
+          @show t1 / t0
+          @testset "newberger, $σ, $kz" begin
+            @test isapprox(summed, newberger)
+          end
+          #t0 = @elapsed LMV.converge(n->LMV.coupledvelocity(coupled, config, n))
+          #@show t1 / t0
+        #catch
+        #end
       end
     end
   end
