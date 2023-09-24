0383c1eb7c18b4866c488b0aa0c3e857ea6f088d
James Cook <cookjws@gmail.com>
2023-09-24 17:09:33 +0100

    forgot begin statement

--- test/tensors/CoupledVsSeparable.jl
+++ test/tensors/CoupledVsSeparable.jl
@@ -29,7 +29,7 @@ Random.seed!(0)
     coupledRingBeam = CoupledVelocitySpecies(Π, Ω, argsR...)
     separableRingBeam = RingBeamSpecies(Π, Ω, argsR...)
 
-    @testset "Ensure besselj composes with complex indices and Duals"
+    @testset "Ensure besselj composes with complex indices and Duals" begin
       @test DualNumbers.dualpart(besselj(1.0 + im, Dual(1.0, 1))) ≈
         (besselj(0+im, 1.0) - besselj(2.0+im, 1.0)) / 2
     end
