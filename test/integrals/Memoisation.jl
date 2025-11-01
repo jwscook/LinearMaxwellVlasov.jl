4bbc2bd79a215e571c65e71a9841802bc6637df0
James Cook <cookjws@gmail.com>
2025-11-01 17:50:46 +0000

    made test/integrals/Memoisation slightly clearer (to me)

--- test/integrals/Memoisation.jl
+++ test/integrals/Memoisation.jl
@@ -16,30 +16,32 @@ Random.seed!(0)
   @testset "Testing perp memoisation logic" begin
     @testset "Maxwellian" begin
       kr, ki = rand(2)
+      @assert kr >= 0
+      @assert ki >= 0
       vth = rand()
       for n in -2:2
         for f in fs
           a = perpendicular(vth, f(kr + im*ki), abs(n))
           for x in f.((kr+im*ki, kr-im*ki, -kr+im*ki, -kr-im*ki))
             cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
-            b = perpendicular(vth, x, n)
-            if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+            expected = perpendicular(vth, x, n)
+            if !all(isapprox.(expected, cp(a), rtol=sqrt(eps())))
               @show n, sign(real(x)), sign(imag(x))
-              @show round.(b ./ cp(a))
+              @show round.(expected ./ cp(a))
             end
-            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-            @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
+            @test all(isapprox.(expected, cp(a), rtol=sqrt(eps())))
+            @test all(isapprox.(expected, cp(cp(expected)), rtol=sqrt(eps())))
           end
           a = perpendicular(vth, f(kr), abs(n))
           for x in f.((-kr, kr))
             cp = CacheOp{MaxwellianPerpendicularCacheOp}(x, n)
-            b = perpendicular(vth, x, n)
-            if !all(isapprox.(b, cp(a), rtol=sqrt(eps())))
+            expected = perpendicular(vth, x, n)
+            if !all(isapprox.(expected, cp(a), rtol=sqrt(eps())))
               @show n, sign(real(x)), sign(imag(x))
-              @show round.(b ./ cp(a))
+              @show round.(expected ./ cp(a))
             end
-            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-            @test all(isapprox.(b, cp(cp(b)), rtol=sqrt(eps())))
+            @test all(isapprox.(expected, cp(a), rtol=sqrt(eps())))
+            @test all(isapprox.(expected, cp(cp(expected)), rtol=sqrt(eps())))
           end
         end
       end
@@ -47,6 +49,8 @@ Random.seed!(0)
     @testset "Generic" begin
       species = RingBeamSpecies(rand(6)...)
       kr, ki = rand(2) # the positive parts
+      @assert kr >= 0
+      @assert ki >= 0
       for n in -2:2
         for f in fs
           x = f(kr + im*ki)
@@ -55,18 +59,18 @@ Random.seed!(0)
           for x in f.((kr+im*ki, -kr+im*ki, kr-im*ki, -kr-im*ki)) # permutations
             cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
             config = Configuration(Wavenumber(kz=1.0, k⊥=x))
-            b = perpendicular(species, config, n)
-            @test all(isapprox.(b, cp(a), rtol=sqrt(eps())))
-            @test all(b .== cp(cp(b)))
+            expected = perpendicular(species, config, n)
+            @test all(isapprox.(expected, cp(a), rtol=sqrt(eps())))
+            @test all(expected .== cp(cp(expected)))
           end
           config = Configuration(Wavenumber(kz=1.0, k⊥=f(kr))) # real positive
           a = perpendicular(species, config, abs(n))
           for x in f.((-kr, kr)) # real only permutations
             config = Configuration(Wavenumber(kz=1.0, k⊥=x))
             cp = CacheOp{GenericPerpendicularCacheOp}(x, n)
-            b = perpendicular(species, config, n)
-            @test all(b .== cp(a))
-            @test all(b .== cp(cp(b)))
+            expected = perpendicular(species, config, n)
+            @test all(expected .== cp(a))
+            @test all(expected .== cp(cp(expected)))
           end
         end
       end
@@ -80,12 +84,12 @@ Random.seed!(0)
             K = Wavenumber(kz=1.0, k⊥=f((B ? kr+im*ki : kr)))
             config = Configuration(K)
             for n ∈ -2:2
-              a = perpendicular(species, config, n)
+              expected = perpendicular(species, config, n)
               perpmem = LMV.constructperpendicular_memoised(species, config)
               for _ ∈ 1:2
                 b = perpmem(species, config, n)
                 c = perpmem(species, config, n)
-                @test all(isapprox.(b, a, rtol=sqrt(eps())))
+                @test all(isapprox.(expected, b, rtol=sqrt(eps())))
                 @test all(b .== c)
               end
             end
